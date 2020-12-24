5228b54a81bb035a0e6abb3e976b631a833c79e9
AM <mouh.bk16@gmail.com>
2020-12-24 02:15:49 +0100

    Finished the tutorial

--- script.js
+++ script.js
@@ -1,49 +1,113 @@
-const startButton = document.querySelector('.start-btn');
-const questionContainerElement= document.querySelector('#question-container');
-let shuffledQuestions, currentQuestionIndex;
-const questionElement = document.querySelector('#question');
-const answerButtonsElement = document.querySelector('#answer-buttons');
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-startButton.addEventListener('click', startGame);
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
-    console.log('started');
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort( () => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion();  
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question;
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+  
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText ='Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct') 
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(elemenet) {
+    elemenet.classList.remove('correct')
+    elemenet.classList.remove('wrong')
 }
 
 const questions = [
-{
-    question: 'What is 2 + 2 ?',
-    answer: [
-        { text: '4', correct: true},
-        { text: '8', correct: false},
-        { text: '3', correct: false},
-        { text: '2', correct: false},
-    ]
-},
-{
-    question: 'What is 2 - 2!',
-    answer: [
-        { text: '0', correct: true},
-        { text: '5', correct: false},
-        { text: '3', correct: false},
-        { text: '2', correct: false},
-    ]
-}
-]
+    {
+      question: 'What is 2 + 2 ?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'What is 2- 4 ?',
+      answers: [
+        { text: '-2', correct: true },
+        { text: '2', correct: false },
+        { text: '6', correct: false },
+        { text: '-6', correct: false }
+      ]
+    },
+    {
+      question: 'Was this tutorial helpful ?',
+      answers: [
+        { text: 'Yes', correct: true },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'A little bit', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '2 * 4', correct: true },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
